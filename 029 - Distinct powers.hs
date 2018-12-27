cad89a3337f4520cbb05533b7e8c4a13b4d9ea95
Pavan Varma <varmamsp111@gmail.com>
2018-12-27 22:37:45 +0530

    Add new problem solution.

---
+++ 029 - Distinct powers.hs
@@ -0,0 +1,15 @@
+module Main where
+  
+import qualified Data.Set as Set
+
+main :: IO ()
+main = print
+     . length
+     . Set.fromList
+     $ [ a ^ b | a <- [2..100]
+               , b <- [2..100] ]
+
+{-
+Author: bumpy (-_-)
+date: 27-Dec-2018
+-}
