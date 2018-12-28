99ceea733699ed5f88c56f1b80a9c4d23ff598b0
Pavan Varma <varmamsp111@gmail.com>
2018-12-29 00:32:20 +0530

    Add new problem solution.

---
+++ 030 - Digit fifth powers.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.Char (digitToInt) 
+
+main :: IO ()
+main = print
+     . sum
+     $ [ x | x <- [2..354294] 
+           , digitSum x == x ]
+  where
+    digitSum = sum 
+             . map ((^5) . digitToInt)
+             . show
+
+{-
+Author: bumpy (-_-)
+date: 28-Dec-2018
+-}
