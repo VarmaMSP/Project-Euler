986b249cd4ca07138b7fbb1c05df16b1d0b5e20f
Pavan Varma <varmamsp111@gmail.com>
2018-12-28 12:22:12 +0530

    Add new problem solution.

---
+++ 033 - Digit cancelling fractions.hs
@@ -0,0 +1,33 @@
+module Main where
+
+main :: IO ()
+main = print
+     . uncurry (\p q -> q `div` gcd p q)
+     . foldl1 (\(p, q) (r, s) -> (p * r, q * s))
+     $ [ (m, n) | a <- [1..9], b <- [1..9], c <- [1..9]
+                , let m = 10 * a + c 
+                , let n = 10 * b + c
+                , m < n && okay m n a b ]
+    ++ [ (m, n) | a <- [1..9], b <- [1..9], c <- [1..9]
+                , let m = 10 * c + a 
+                , let n = 10 * c + b
+                , m < n && okay m n a b ]
+    ++ [ (m, n) | a <- [1..9], b <- [1..9], c <- [1..9]
+                , let m = 10 * c + a
+                , let n = 10 * b + c
+                , m < n && okay m n a b ]
+    ++ [ (m, n) | a <- [1..9], b <- [1..9], c <- [1..9]
+                , let m = 10 * a + c
+                , let n = 10 * c + b
+                , m < n && okay m n a b ]
+  where
+    okay m n a b = 
+      let p = gcd m n 
+          q = gcd a b 
+      in (m `div` p) == (a `div` q) && 
+         (n `div` p) == (b `div` q)
+
+{-
+Author: bumpy (-_-)
+date: 28-Dec-2018
+-}
